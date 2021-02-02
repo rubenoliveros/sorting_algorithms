baf7e0b9b06bcf28b4393d6b0b9abd028c8d97f9
Ruben Oliveros <ruben.oliveros@gmail.com>
2021-02-02 04:42:08 +0000

    First version

---
+++ 2-selection_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * selection_sort - a function that sorts an array of integers in ascending
+ * order using the Selection sort algorithm
+ *
+ * @array: The array to sort
+ * @size: The size of the array
+ *
+ *
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int tmp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (i != min)
+		{
+			tmp = array[i];
+			array[i] = array[min];
+			array[min] = tmp;
+			print_array(array, size);
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+3-quick_sort.c 3-O 2-selection_sort.c 2-O 1-insertion_sort_list.c 1-O 0-bubble_sort.c 0-O -
